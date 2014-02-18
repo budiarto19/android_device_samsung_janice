e2f08c3a33cad2936e7cd072dae72845056d6c2d
D4X <t2plsa2gmail.com>
2014-02-18 22:59:17 +0300

    Add support for janice

--- mahdi.mk
+++ mahdi.mk
@@ -1,20 +1,16 @@
 # Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/mahdi/config/gsm.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := GT-I9070
 
-# Boot animation
-TARGET_SCREEN_HEIGHT := 800
-TARGET_SCREEN_WIDTH := 480
+# Inherit common product files.
+$(call inherit-product, vendor/mahdi/configs/common.mk)
+$(call inherit-product, vendor/mahdi/configs/common_full_phone.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-# If the boot animation is not automatically selected, we chose it here :)
-
-PRODUCT_COPY_FILES +=  \
-    vendor/cm/prebuilt/common/bootanimation/480.zip:system/media/bootanimation.zip
+# Copy device specific prebuilt files.
+PRODUCT_COPY_FILES += \
+    vendor/mahdi/prebuilt/bootanimations/BOOTANIMATION-800x480.zip:system/media/bootanimation.zip
 
 
 # Inherit device configuration
@@ -22,7 +18,7 @@ $(call inherit-product, device/samsung/janice/full_janice.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := janice
-PRODUCT_NAME := cm_janice
+PRODUCT_NAME := mahdi_janice
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := GT-I9070
 PRODUCT_MANUFACTURER := samsung
